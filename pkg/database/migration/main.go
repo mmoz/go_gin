e040e1b62f85bf48afe50e7d056d2c392304a2a5
zackmmo <tachapoom@isoftel.co.th>
2023-11-30 11:15:11 +0700

    migration

--- pkg/database/migration/main.go
+++ pkg/database/migration/main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -27,23 +29,43 @@ func main() {
 	}
 	defer db.Close()
 
-	// Resolve the absolute path to the migration file
-	absPath, err := filepath.Abs("./pkg/database/migration/create_users_table.up.sql")
+	// Resolve the absolute path to the migration directory
+	migrationDir := "./pkg/database/migration"
+	absPath, err := filepath.Abs(migrationDir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Absolute Path to Migration:", absPath)
+	fmt.Println("Absolute Path to Migration Directory:", absPath)
 
-	// Read the content of the SQL file
-	sqlContent, err := ioutil.ReadFile(absPath)
+	// Read all files in the migration directory
+	files, err := ioutil.ReadDir(absPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Execute the SQL commands
-	_, err = db.Exec(string(sqlContent))
-	if err != nil {
-		log.Fatal(err)
+	// Sort files based on their names
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].Name() < files[j].Name()
+	})
+
+	// Execute the SQL commands for each file
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".up.sql") {
+			filePath := filepath.Join(absPath, file.Name())
+			fmt.Printf("Applying migration from file: %s\n", filePath)
+
+			// Read the content of the SQL file
+			sqlContent, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Execute the SQL commands
+			_, err = db.Exec(string(sqlContent))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 
 	fmt.Println("Migration successful")
